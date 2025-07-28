6693aac3a687149ca85cddcb1fd2f26080ea2171
KunusothNithin <rakeshkantathmakur4u@gmail.com>
2025-07-28 22:39:29 +0530

    Add Admin panel routes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -61,7 +61,6 @@ const getAllOrders = async (req, res) => {
 const getOrdersByUser = async (req, res) => {
     try{
         const userId = req.params.id;
-        console.log(userId);
         const orders = await Order.find({user: userId});
         res.status(200).json(orders);
     }catch(error){
